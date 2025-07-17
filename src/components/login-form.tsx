fbf2e9be069d2922e8732804df98c5dd8ad9475b
juandavidg0mez <nihlgomez@gmail.com>
2025-07-17 09:05:44 -0500

    Merge con rama eliDev

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -66,8 +66,8 @@ export function EnhancedLoginForm({
       setProgress(90)
 
       const region = userPoolId.split("_")[0] || "us-east-1"
-      const redirectUri = "https://appui.d1ajb21hsxi2dm.amplifyapp.com/api/auth/callback"
-      // const redirectUri = "http://localhost:3000/api/auth/callback"
+      //const redirectUri = "https://appui.d1ajb21hsxi2dm.amplifyapp.com/api/auth/callback"
+      const redirectUri = "http://localhost:3000/api/auth/callback"
 
       const scope = "email+openid+profile"
 
