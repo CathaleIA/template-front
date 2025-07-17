44e33c0b2fc15226ab397061999d4c6b8568d9de
HaderCabrera <hader.cabrera96@gmail.com>
2025-07-17 11:50:40 -0500

    select-tenant OK

--- src/app/select-tenant/page.tsx
+++ src/app/select-tenant/page.tsx
@@ -1,17 +1,17 @@
-import { EnhancedLoginForm } from "@/components/login-form"
+import { SelectTenant } from "@/components/select-tenant-form"
 
 export default function LoginPage() {
   return (
-<div 
-  className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
-  style={{ 
-    backgroundImage: 'var(--background-image)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  }}
->
+    <div
+      className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
+      style={{
+        backgroundImage: 'var(--background-image)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
       <div className="w-full max-w-sm md:max-w-3xl">
-        <EnhancedLoginForm />
+        <SelectTenant />
       </div>
     </div>
   )
