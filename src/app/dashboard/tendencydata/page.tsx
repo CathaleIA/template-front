bd77524a26fff000ca302c04a2cd8fac84b9a5b8
cathaleia <eficiencia.desarrollo@copower.com.co>
2025-09-01 10:12:44 -0500

    todo OK, solo colores

--- src/app/dashboard/tendencydata/page.tsx
+++ src/app/dashboard/tendencydata/page.tsx
@@ -7,6 +7,8 @@ import {
     TabsTrigger,
 } from "@/components/ui/tabs"
 
+import { SidebarMenu, SidebarMenuItem, SidebarMenuSkeleton } from "@/components/ui/sidebar"
+
 import GeneratorComponent from "@/components/tendency-navigation/GeneratorComponent"
 import MotorGridComponent from "@/components/tendency-navigation/MotorComponent"
 import HealhtlyComponent from "@/components/tendency-navigation/TermodinamicComponente"
@@ -15,10 +17,21 @@ import ActivosPage from "@/app/dashboard/activo/page"
 import { useState } from "react"
 
 export default function TabsDemo() {
-    const [activeTab, setactiveTab]  =useState('motor');
+    const [activeTab, setactiveTab] = useState('motor');
     return (
         <div className="[--tablist-height:89px] flex flex-col pt-2">
             MAPA O CROQUIZ DE LOS SISTEMAS EN GENERAL DEL CLIENTE
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuSkeleton showIcon />
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                    <SidebarMenuSkeleton showIcon />
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                    <SidebarMenuSkeleton showIcon />
+                </SidebarMenuItem>
+            </SidebarMenu>
         </div>
     )
 }
