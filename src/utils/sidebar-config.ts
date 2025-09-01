55b5de33f001507f13921dc0eba15338af313835
juandavidg0mez <nihlgomez@gmail.com>
2025-09-01 10:33:29 -0500

    Revert "Add sideVar reports"
    This reverts commit 159966de84bc28d89376387a9ff43be9d2457e4c.

--- src/utils/sidebar-config.ts
+++ src/utils/sidebar-config.ts
@@ -30,7 +30,7 @@ export const SIDEBAR_BY_TENANT: Record<string, ProjectData> = {
       // ],
       [
         { "title": "Reports", "url": "#" },
-        { "title": "Create", "url": "/dashboard/reports/principal" },
+        { "title": "Create", "url": "/dashboard/reports" },
         { "title": "List", "url": "/dashboard/listreports" },
         { "title": "Config", "url": "#" },
       ],
@@ -71,7 +71,7 @@ export const SIDEBAR_BY_TENANT: Record<string, ProjectData> = {
     tree: [
       [
         { "title": "Reports", "url": "#" },
-        { "title": "Create", "url": "/dashboard/reports/items" },
+        { "title": "Create", "url": "/dashboard/reports" },
         { "title": "List", "url": "/dashboard/listreports" },
         { "title": "Config", "url": "#" },
 
