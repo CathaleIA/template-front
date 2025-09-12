4dd5e213799c2583ee54f2ad45949bfc386cf7cd
juandavidg0mez <nihlgomez@gmail.com>
2025-09-11 22:38:59 -0500

    Integracion graficas botones y data-table

---
+++ src/utils/reports-utils/generate-grafics.ts
@@ -0,0 +1,1 @@
+// toma como parametro una imagen y la pasa a bas464
---
+++ src/utils/reports-utils/graficas-base64.ts
@@ -0,0 +1,2 @@
+// toma como parametro el json y el nombre de la grafica y crea la imagen
+// implementa la funcion de generate-grafics
