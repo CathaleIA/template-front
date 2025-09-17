d6b2fcfb44fb99143dab3cf6bf2819690cdbc3dc
cathaleia <eficiencia.desarrollo@copower.com.co>
2025-09-17 16:55:46 -0500

    optimizando login

--- src/components/select-tenant-form.tsx
+++ src/components/select-tenant-form.tsx
@@ -77,7 +77,8 @@ export function SelectTenant() {
       setProgress(90)
 
       const region = userPoolId.split("_")[0] || "us-east-1"
-      const redirectUri = "http://localhost:3000/api/auth/callback"
+      const redirectUri = "https://appui.d1ajb21hsxi2dm.amplifyapp.com/api/auth/callback"
+      // const redirectUri = "http://localhost:3000/api/auth/callback"
       const scope = "email+openid+profile"
       const state = encodeURIComponent(
         JSON.stringify({ userPoolId, appClientId: clientId, userPoolDomain })
