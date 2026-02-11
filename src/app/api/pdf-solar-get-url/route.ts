82371e7a75191c204b70db44a6e774a0d3c2d1b0
juandavidg0mez <nihlgomez@gmail.com>
2026-02-11 14:35:06 -0500

    Create Buttom pre signURL and runtime

--- src/app/api/pdf-solar-get-url/route.ts
+++ src/app/api/pdf-solar-get-url/route.ts
@@ -1,3 +1,5 @@
+
+export const runtime = "nodejs";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextResponse } from "next/server";
